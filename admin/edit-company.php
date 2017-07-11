ce512ca5e4a4f0e6f22a5bccbd71666fe048037d
MughalMirza <tiger.mirza99@gmail.com>
2017-07-11 15:10:56 +0500

    updated company and database links in ajax

--- admin/edit-company.php
+++ admin/edit-company.php
@@ -197,13 +197,13 @@ include "header.php";
 
                                         <div class="form-group">
                                             <label>Ordering Deadline Time </label>
-                                            <input class="form-control" id="ordering_deadline_time" name="ordering_deadline_time" placeholder="Ordering Deadline" type="text">
+                                            <input class="form-control" id="ordering_deadline_time" name="ordering_deadline_time" placeholder="Ordering Deadline" type="text" value="<?=$edit_company['ordering_deadline_time'];?>">
                                             <span style="font-size: 14px; color: red; width: 100%;text-align: left; padding: 9px;text-transform: none;" id="error_ordering_deadline_time"></span>
                                         </div>
 
                                         <div class="form-group">
                                             <label>Delivery Time </label>
-                                            <input class="form-control" id="delivery_time" name="delivery_time" placeholder="Delivery Time" type="text">
+                                            <input class="form-control" id="delivery_time" name="delivery_time" placeholder="Delivery Time" type="text" value="<?=$edit_company['delivery_time'];?>">
                                             <span style="font-size: 14px; color: red; width: 100%;text-align: left; padding: 9px;text-transform: none;" id="error_delivery_time"></span>
                                         </div>
 
