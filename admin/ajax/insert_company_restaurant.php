ebbfd5202fe05f6927cd933041fbed05e94cd049
ahmadworkspace <ahmadworkspace@gmail.com>
2017-07-11 12:49:30 +0500

    - First Commit 11 JULY

--- admin/ajax/insert_company_restaurant.php
+++ admin/ajax/insert_company_restaurant.php
@@ -2,10 +2,10 @@
 require_once '../inc/initDb.php';
 $company_id = $_POST['company_id'];
 $url = $_POST['url'];
-DB::useDB('orderapp_b2b');
+DB::useDB('orderapp_b2b_wui');
 foreach ($_POST['rest_name'] as $id)
 {
-    DB::useDB('orderapp_b2b');
+    DB::useDB('orderapp_b2b_wui');
 
     DB::insert('company_rest', array(
         "company_id" => $company_id,
